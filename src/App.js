dadf8a048e32ab9a4e0d51491ed3e0ce354a3432
Brian Wilson <bwilson@co.clatsop.or.us>
2018-11-09 10:08:42 -0800

    Added a few comments.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './App.css';
 
+// Define a new React component.
 class HelloMessage extends React.Component {
   render() {
       return <div>Hello, your name is probably not {this.props.name}.</div>;
   }
 }
 
+// Mount the component onto the DOM in the div known as "app".
 let mountNode = document.getElementById("app");
 ReactDOM.render(<HelloMessage name="Brian" />, mountNode);
+
+// That's all!
