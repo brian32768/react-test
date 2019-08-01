64d7b2023bf64f89ad391bf06be096f3513542eb
Brian H Wilson <bwilson@co.clatsop.or.us>
2019-07-31 17:05:19 -0700

    added react-simplemap

---
+++ src/mappage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Map from '@map46/react-simplemap'
+
+const MapPage = () => {
+    return (
+        <>
+            Eventually, every app I write ends up with a map in it.
+            <Map style={{position:'relative',top:0,width:600,height:400,padding:10}}/>
+        </>
+    )
+}
+export default MapPage;
