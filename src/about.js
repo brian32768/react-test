835a3dbd4fccbb9035bc199dec692b596b250d84
Brian H Wilson <brian@wildsong.biz>
2019-07-31 20:24:07 -0700

    made function

--- src/about.js
+++ src/about.js
@@ -1,36 +1,22 @@
-import React, {Component} from "react"
+import React from "react"
 import PropTypes from 'prop-types'
-import ReactDOM from "react-dom"
 
-class TryMe extends Component {
-    static propTypes = {
-        text: PropTypes.string.isRequired
-    };
-
-    render() {
-        return (
-            <>
-            <h3>{this.props.text}</h3>
-            {this.props.children}
-            </>
-        )
-    }
+const TryMe = (props) => {
+    return (
+        <>
+            <h3>{props.text}</h3>
+            {props.children}
+        </>
+    )
 }
 
-class About extends Component {
-    static propTypes = {
-        text: PropTypes.string.isRequired
-    };
-
-    render() {
-        console.log("It's about props.", this.props.match.params);
-        return (
-            <div id="about">
+const About = (props) => {
+    console.log("It's about props.", props);
+    return (
+        <div id="about">
             <h2>About react-test</h2>
-            <TryMe text={this.props.text}>This is my 14 day free trial</TryMe>
-            </div>
-        );
-    }
+            <TryMe text={props.text}>This is my 14 day free trial</TryMe>
+        </div>
+    );
 }
-
 export default About;
